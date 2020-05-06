48d457894161867f97255021aad62e8875d5171f
dragos-vlasie <vlasiedragos1@gmail.com>
2020-05-06 21:30:18 +0100

    Update Dashboard.js

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -21,9 +21,13 @@ export const Dashboard = () => {
         <div className="col s12 center-align">
           <h4>
             <b>Hey there,</b>
-            <p className="flow-text grey-text text-darken-1">
+            <p
+              className="flow-text grey-text text-darken-1"
+              style={{ marginTop: "14.5px" }}
+            >
               Welcome to Proudly app
             </p>
+            <b>Here you can see the other users currently using the app</b>
           </h4>
         </div>
       </div>
